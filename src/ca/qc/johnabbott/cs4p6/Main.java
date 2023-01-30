b04c4b3ffba29007f41ac061fce1b4f94810f8c0
NitpreetA <nitha6718@gmail.com>
2023-01-30 10:50:29 -0500

    abc

--- src/ca/qc/johnabbott/cs4p6/Main.java
+++ src/ca/qc/johnabbott/cs4p6/Main.java
@@ -24,6 +24,7 @@ Main {
                     System.out.println(newLine);
                 }
 
+
         }
         catch (IOException e){
             throw  new IOException( e.getMessage());
--- src/ca/qc/johnabbott/cs4p6/Quest.java
+++ src/ca/qc/johnabbott/cs4p6/Quest.java
@@ -1,5 +1,5 @@
 package ca.qc.johnabbott.cs4p6;
 
 public class Quest {
-
+    private int _gold;
 }
